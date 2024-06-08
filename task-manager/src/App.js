ed552d4762d037e9e9ea60affc25f0c6342a9784
Vince Nguyen <vince.ngu@berkeley.edu>
2024-06-08 00:20:26 -0700

    Inital Commit Test

--- task-manager/src/App.js
+++ task-manager/src/App.js
@@ -221,7 +221,7 @@ export default function Example() {
                 </a>
               </div>
               <h1 className="mt-10 text-4xl font-bold tracking-tight text-white sm:text-6xl">
-                Deploy to the cloud with confidence
+                Initial Commit
               </h1>
               <p className="mt-6 text-lg leading-8 text-gray-300">
                 Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
